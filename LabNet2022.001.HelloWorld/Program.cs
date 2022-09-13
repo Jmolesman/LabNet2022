0a116fd15784dda46fc6bbfe176ac0951037b696
Topo <juanmatiasojeda@gmail.com>
2022-09-13 00:26:38 -0300

    Se sube código del Proyecto HelloWorld
    Se sube el código y la solución del primer proyecto del Laboratorio de .Net "HelloWorld"

---
+++ LabNet2022.001.HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet2022._001.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
